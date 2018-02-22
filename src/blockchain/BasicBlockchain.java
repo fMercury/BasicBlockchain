26055354c20de1b13ccd013a5c31374830aa2d08
Franco <fberdun@gmail.com>
2018-02-22 14:38:32 -0300

    BasicBlockchain Java for Workshop

---
+++ src/blockchain/BasicBlockchain.java
@@ -0,0 +1,65 @@
+package blockchain;
+import java.util.ArrayList;
+import java.util.Date;
+import com.google.gson.GsonBuilder;
+
+public class BasicBlockchain {
+
+	/*6*/
+	public static ArrayList<Block> blockchain = new ArrayList<Block>(); 
+	
+	//Block Constructor.
+	public BasicBlockchain( ) {
+			
+	}
+	
+	/*5*//*
+	public static void main(String[] args) {
+		
+		Block genesisBlock = new Block("Hi im the first block", "0");
+		System.out.println("Hash for block 1 : " + genesisBlock.hash);
+		
+		Block secondBlock = new Block("Yo im the second block",genesisBlock.hash);
+		System.out.println("Hash for block 2 : " + secondBlock.hash);
+		
+		Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
+		System.out.println("Hash for block 3 : " + thirdBlock.hash);
+		
+	}
+	*/
+	
+	/*7*/
+	public static void main(String[] args) {	
+		//add our blocks to the blockchain ArrayList:
+		blockchain.add(new Block("Hi im the first block", "0"));		
+		blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash)); 
+		blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+		
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+		System.out.println(blockchainJson);
+	}
+	
+	/*8*/
+	public static Boolean isChainValid() {
+		Block currentBlock; 
+		Block previousBlock;
+		
+		//loop through blockchain to check hashes:
+		for(int i=1; i < blockchain.size(); i++) {
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i-1);
+			//compare registered hash and calculated hash:
+			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+				System.out.println("Current Hashes not equal");			
+				return false;
+			}
+			//compare previous hash and registered previous hash
+			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+		}
+		return true;
+	}
+	
+}
---
+++ src/blockchain/Block.java
@@ -0,0 +1,33 @@
+package blockchain;
+
+import java.util.Date;
+
+/*1*/
+public class Block {
+
+	public String hash;
+	public String previousHash;
+	private String data; //our data will be a simple message.
+	private long timeStamp; //as number of milliseconds since 1/1/1970.
+
+	//Block Constructor.
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		
+		/*4*/
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+		
+	}
+	
+	/*3*/
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				data 
+				);
+		return calculatedhash;
+	}
+}
